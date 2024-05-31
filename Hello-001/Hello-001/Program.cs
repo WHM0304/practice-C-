f81e9202f7976a2e712e2191d10dcd9533102961
WHM0304 <whm0304@naver.com>
2024-05-31 10:17:09 +0900

    first C#

---
+++ Hello-001/Hello-001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First C# PROJECT");
+            Console.WriteLine(3 + 3);
+            Console.Write("write");
+            Console.Write(" is not insert a line");
+            Console.Write(" ex) JAVA 의 print");
+        }
+    }
+}
---
+++ Hello-003/Hello-003/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hello_003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            String name = "Hong";
+            Console.WriteLine(name + " STRING 변수 !");
+        }
+    }
+}
